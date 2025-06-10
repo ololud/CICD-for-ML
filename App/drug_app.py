281249d45dd9c9e1e5d4f5617efc822099e6a83f
Olaf Ludwig <olaflud@yahoo.com>
2025-06-10 19:59:26 +0200

    cicd3

--- App/drug_app.py
+++ App/drug_app.py
@@ -2,7 +2,7 @@ import gradio as gr
 import skops.io as sio
 
 pipe = sio.load(
-    "../Model/drug_pipeline.skops",
+    "Model/drug_pipeline.skops",
     trusted=[
         "sklearn.pipeline.Pipeline",
         "sklearn.preprocessing._data.StandardScaler",
